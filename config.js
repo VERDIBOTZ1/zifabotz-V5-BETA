0879a75304e72dd7b4ccad958e69b597e2f5eb58
kurang turu <95448883+VERDIBOTZ1@users.noreply.github.com>
2022-05-19 14:07:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,9 @@
 // AlyaaXzy
 // BOTCAHX
 // Zifabotz
-
+//VERDIBOTZ1
 let fs = require('fs')
-global.owner = ['6285828530078', '6285828530078','6285828764046', '6285828764046', '6285828764046'] // Letakan nomor kamu disini
+global.owner = ['6285706735450', '6285706735450','6283869803330', '6283869803330', '0'] // Letakan nomor kamu disini
 global.APIs = { // API Prefix
   // nama: 'https://website'
   hardianto: 'https://hardianto-chan.herokuapp.com',
@@ -41,13 +41,13 @@ global.APIKeys = { // APIKey nya disini
 }
 
 // Sticker WM
-global.packname = 'rozi'
-global.author = 'zifa'
+global.packname = 'VERDI'
+global.author = 'MEx'
 
 global.wait = '▰▰▰▰▱▱▱▱▱▱ ʟᴏᴀᴅɪɴɢ...'
-global.eror = '「❗」Server LAGI ERROR COK!'
+global.eror = '「❗」Server LAGI ERROR anj!'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
-global.watermark = '© zifabotz' //change the watermark 
+global.watermark = '©VERDIBORTZ1' //change the watermark 
 global.image = 'https://telegra.ph/file/052ca138a613c8859756f.jpg' //change the image
 global.thumbfoto = 'https://telegra.ph/file/c3bcc6f8e8574d1c5274f.jpg'
 
